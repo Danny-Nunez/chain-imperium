690cb39b1e35c4f55cce14f254acf1d155921764
Danny Nunez <dnunez22@gmail.com>
2023-04-26 18:37:43 -0400

    updates

--- src/pages/products/products-dark.jsx
+++ src/pages/products/products-dark.jsx
@@ -4,11 +4,15 @@ import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import DarkTheme from "../../layouts/Dark";
 import { checkout } from "../../common/checkout";
+import confetti from 'canvas-confetti';
+import { useRouter } from 'next/router';
 
 const Products = () => {
   const fixedHeader = React.useRef(null);
   const MainContent = React.useRef(null);
   const navbarRef = React.useRef(null);
+  const router = useRouter();
+  const { status } = router.query;
 
 
   React.useEffect(() => {
@@ -44,6 +48,20 @@ const Products = () => {
       <div className="main-content" ref={MainContent}>
       <div className="prServiceWrapper">
         <div className="prContainer">
+
+        {status && status === 'success' && (
+  <div className='bg-green-100 text-green-700 p-2 rounded border mb-2 border-green-700'>
+    <h3>Thank you, Payment Successful.</h3>
+     You will receive an email shortly with the info needed to complete your pr placement request.
+  </div>
+)}
+{status && status === 'cancel' && (
+  <div className='bg-red-100 text-red-700 p-2 rounded border mb-2 border-red-700'>
+    <h3>Something went wrong please try again</h3>
+    Payment Unsuccessful
+  </div>
+)}
+
           <div className="prServiceCard">
             <img className="productImage" src="/img/yahoo.png" alt="" />
             <p><h6>1 PR Placement</h6></p>
