71ac9f83520dbef49f11fb07a3c85b1b6a00f629
Danny Nunez <dnunez22@gmail.com>
2023-04-27 11:49:42 -0400

    updates

--- src/common/navbar.js
+++ src/common/navbar.js
@@ -2,33 +2,56 @@ import { getSiblings, fadeIn, fadeOut } from "./customFunctions";
 
 export const handleDropdown = (e) => {
   const parentElement = e.target.parentElement;
-  if (parentElement) {
-    getSiblings(parentElement)
-      .filter((item) => item.classList.contains("show"))
-      .map((item) => {
-        item.classList.remove("show");
-        if (item.childNodes[0]) {
-          item.childNodes[0].setAttribute("aria-expanded", false);
-        }
-        if (item.childNodes[1]) {
-          item.childNodes[1].classList.remove("show");
-        }
-      });
-    parentElement.classList.toggle("show");
-    e.target.setAttribute("aria-expanded", true);
-    const childNodes = parentElement.childNodes;
-    if (childNodes.length > 1 && childNodes[1].classList) {
-      childNodes[1].classList.toggle("show");
-    }
+  if (!parentElement) return;
+
+  const siblings = getSiblings(parentElement);
+  if (!siblings) return;
+
+  siblings
+    .filter((item) => item.classList && item.classList.contains("show"))
+    .map((item) => {
+      item.classList.remove("show");
+      if (item.childNodes[0]) {
+        item.childNodes[0].setAttribute("aria-expanded", false);
+      }
+      if (item.childNodes[1]) {
+        item.childNodes[1].classList.remove("show");
+      }
+    });
+  
+  parentElement.classList.toggle("show");
+  e.target.setAttribute("aria-expanded", true);
+  if (parentElement.childNodes[1]) {
+    parentElement.childNodes[1].classList.toggle("show");
   }
 };
 
 export const handleMobileDropdown = (e) => {
   const navbarElement = document.getElementById("navbarSupportedContent");
-  if (navbarElement && navbarElement.classList) {
-    navbarElement.classList.toggle("show-with-trans");
+  if (!navbarElement) return;
+  
+  navbarElement.classList.toggle("show-with-trans");
+};
+
+export const handleSearch = () => {
+  const searchForm = document.querySelector(".navbar");
+  if (!searchForm) return;
+  
+  const iconElement = document.querySelector(".navbar .icon");
+  if (iconElement) {
+    iconElement.addEventListener("click", function () {
+      // handle click event
+    });
+  }
+
+  const closeElement = document.querySelector(".navbar .close");
+  if (closeElement) {
+    closeElement.addEventListener("click", function () {
+      // handle click event
+    });
   }
 };
+
 
 
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -100,9 +100,6 @@ const Navbar = ({ lr, nr, theme }) => {
                 <Link href="/products/products-dark">
                   <a className="dropdown-item">PR Services</a>
                 </Link>
-                <Link href="/products/products-dark">
-                <a className="nav-link">Test</a>
-              </Link>
                 {/* <Link href="/works2/works2-dark">
                   <a className="dropdown-item">Masonry 3 Columns</a>
                 </Link>
