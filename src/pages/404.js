896f2d5debb1c4d96e215f3cd9b720d7c3120bea
Danny Nunez <dnunez22@gmail.com>
2023-05-16 19:39:53 -0400

    updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,16 @@
+import React from "react";
+import Lottie from "react-lottie";
+import animationData from "../../public/img/404.json"
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return <div className="NoPageWrap"><Lottie options={defaultOptions}/><h3>Oops page not found!</h3></div>
   }
