5210e34623f7269cf97d2e366357530bc2aeed63
Danny Nunez <dnunez22@gmail.com>
2023-05-17 13:31:43 -0400

    updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Head from "next/head";
+import Script from "next/script";
 import Lottie from "react-lottie";
 import animationData from "../../public/img/404.json"
-
+import Navbar from "../components/Navbar";
+import DarkTheme from '../layouts/Dark'
 
 const defaultOptions = {
   loop: true,
@@ -13,6 +16,40 @@ const defaultOptions = {
 };
 
 export default function Custom404() {
-    return <div className="NoPageWrap"><div className="lottieWrap"><Lottie options={defaultOptions} height={450}
+    return (
+    <>
+    <DarkTheme>
+    <Head>
+        <title>Chain Imperium</title>
+        <link rel="icon" href="/img/favicon.ico" />
+      </Head>
+      <Navbar />
+    <div className="NoPageWrap"><div className="lottieWrap"><Lottie options={defaultOptions} height={450}
     width={700}/></div><div className="NoPageTitle">Oops page not found! </div></div>
-  }
+
+<Script
+        strategy="beforeInteractive"
+        id="wow"
+        src="/js/wow.min.js"
+      >
+      </Script>
+      <Script
+        strategy="beforeInteractive"
+        id="splitting"
+        src="/js/splitting.min.js"
+      ></Script>
+      <Script id="simpleParallax" src="/js/simpleParallax.min.js"></Script>
+      <Script
+        strategy="beforeInteractive"
+        id="isotope"
+        src="/js/isotope.pkgd.min.js"
+      ></Script>
+      <Script
+        strategy="lazyOnload" 
+        id="initWow" 
+        src="/js/initWow.js"
+      ></Script>
+      </DarkTheme>
+    </>
+    );
+  };
