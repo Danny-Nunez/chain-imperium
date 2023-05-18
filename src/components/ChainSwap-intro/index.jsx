b33fbee2bd76b45ec38ce795c29398ef126ceb83
Danny Nunez <dnunez22@gmail.com>
2023-05-18 16:10:20 -0400

    updates

--- src/components/ChainSwap-intro/index.jsx
+++ src/components/ChainSwap-intro/index.jsx
@@ -10,23 +10,38 @@ const ChainSwapIntro = () => {
     <section className="intro-section section-padding pb-0">
       <div className="container">
         <div className="row">
-          <div className="col-lg-3pt2 col-md-4">
+          {/* <div className="col-lg-3pt2 col-md-4">
             <div className="htit sm-mb30">
+              
               <h4>ChainSwaps</h4>
-              <p>Chainswap provides a decentralized and inclusive trading environment, presenting opportunities for liquidity provision and incorporating innovative features that have bolstered its prominence within the decentralized finance (DeFi) ecosystem. It enables users to generate income through personal swap setups and establish wallet configurations to collect fees. One prominent challenge faced by many startup cryptocurrencies is showcasing their coin logo on Uniswap; however, Chainswap effectively resolves this issue. Additionally, Chainswap facilitates seamless token integration by supporting a broad spectrum of tokens, encompassing established ERC-20 tokens as well as emerging ones. As a result, users gain access to a diverse array of cryptocurrency assets and can actively engage with burgeoning token ecosystems.
+              
+                <p >
+            Chainswap provides a decentralized and inclusive trading environment, presenting opportunities for liquidity provision and incorporating innovative features that have bolstered its prominence within the decentralized finance (DeFi) ecosystem. It enables users to generate income through personal swap setups and establish wallet configurations to collect fees. One prominent challenge faced by many startup cryptocurrencies is showcasing their coin logo on Uniswap; however, Chainswap effectively resolves this issue. Additionally, Chainswap facilitates seamless token integration by supporting a broad spectrum of tokens, encompassing established ERC-20 tokens as well as emerging ones. As a result, users gain access to a diverse array of cryptocurrency assets and can actively engage with burgeoning token ecosystems.
 
               </p>
+              
+            </div>
+          </div> */}
+
+          <div className="col-lg-3pt2 col-md-4">
+            <div className="text">
+              <Split>
+              <h4>ChainSwap</h4>
+                <p className="wow txt words chars splitting" data-splitting>
+                Chainswap provides a decentralized and inclusive trading environment, presenting opportunities for liquidity provision and incorporating innovative features that have bolstered its prominence within the decentralized finance (DeFi) ecosystem. It enables users to generate income through personal swap setups and establish wallet configurations to collect fees. One prominent challenge faced by many startup cryptocurrencies is showcasing their coin logo on Uniswap; however, Chainswap effectively resolves this issue. Additionally, Chainswap facilitates seamless token integration by supporting a broad spectrum of tokens, encompassing established ERC-20 tokens as well as emerging ones. As a result, users gain access to a diverse array of cryptocurrency assets and can actively engage with burgeoning token ecosystems.
+                </p>
+              </Split>
+
             </div>
           </div>
+
+
+
           <div className="col-lg-8pt2 offset-lg-1 col-md-8">
             <div className="text">
               <Split>
                 <p className="wow txt words chars splitting" data-splitting>
                 <div className="Uniswap">
-                <div>
-        
-      </div>   
-          
                   <iframe className="iframe-container" src="https://chainswap.vercel.app/"  />
                 </div>
                 </p>
