cfa46d688778a6190348f84a1ac3b4e5d6da3e3c
Danny Nunez <dnunez22@gmail.com>
2023-05-24 21:12:41 -0400

    updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,23 +7,20 @@ import FacebookChat from "../components/FacebookChat";
 import LoadingScreen from "../components/Loading-Screen";
 import "../styles/globals.css";
 
-
 function MyApp({ Component, pageProps }) {
-
   return (
     <>
       <Head>
         <title>Crafting Blockchain and Web3 Solutions | Chain Imperium</title>
-        <meta name="description" content="Chain Imperium offers blockchain development services, smart contract architectures, NFT marketplace creation, Metaverse development, and more. Contact us for a consultation." />
+        <meta
+          name="description"
+          content="Chain Imperium offers blockchain development services, smart contract architectures, NFT marketplace creation, Metaverse development, and more. Contact us for a consultation."
+        />
         <link rel="icon" href="/img/favicon.ico" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H35NWP06V7"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-H35NWP06V7');
-        </script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-H35NWP06V7"
+        ></script>
       </Head>
       <Cursor />
       <LoadingScreen />
@@ -31,30 +28,24 @@ function MyApp({ Component, pageProps }) {
       <ScrollToTop />
       <Component {...pageProps} />
 
-      <Script
-        strategy="beforeInteractive"
-        id="wow"
-        src="/js/wow.min.js"
-      >
-      </Script>
-      <Script
-        strategy="beforeInteractive"
-        id="splitting"
-        src="/js/splitting.min.js"
-      ></Script>
-      <Script id="simpleParallax" src="/js/simpleParallax.min.js"></Script>
-      <Script
-        strategy="beforeInteractive"
-        id="isotope"
-        src="/js/isotope.pkgd.min.js"
-      ></Script>
-      <Script
-        strategy="lazyOnload" 
-        id="initWow" 
-        src="/js/initWow.js"
-      ></Script>
+      <Script strategy="beforeInteractive" id="wow" src="/js/wow.min.js" />
+      <Script strategy="beforeInteractive" id="splitting" src="/js/splitting.min.js" />
+      <Script id="simpleParallax" src="/js/simpleParallax.min.js" />
+      <Script strategy="beforeInteractive" id="isotope" src="/js/isotope.pkgd.min.js" />
+      <Script strategy="lazyOnload" id="initWow" src="/js/initWow.js" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H35NWP06V7');
+          `,
+        }}
+      />
     </>
   );
 }
 
 export default MyApp;
+
